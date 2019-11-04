70c65bf2df8e642256853bb71238c62cdd1b023e
Kelly Small <xcxkelly98@gmail.com>
2019-11-04 11:35:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
- find_element_index
+
 end
 
 def find_max_value(array)
-  # Add your solution here
+  find_max_value([1,3,5,2,1,3,3,5,7,8,])
 end
 
 def find_min_value(array)
-  # Add your solution here
+ find_min_value([3,2,4,7,3,4,6,1])
 end
