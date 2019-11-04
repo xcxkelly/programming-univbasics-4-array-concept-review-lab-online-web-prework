cc42deedb7570834cc382709cc84d539a1e65dde
Kelly Small <xcxkelly98@gmail.com>
2019-11-04 11:39:44 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,12 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+Disney_Movies = ["frozen", "brave", "Brother bear", "wreck it ralph"]
+find_element_index(3)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  find_max_value([1,3,5,2,1,3,3,5,7,8,])
 end
 
 def find_min_value(array)
-  # Add your solution here
+ find_min_value([3,2,4,7,3,4,6,1])
 end
